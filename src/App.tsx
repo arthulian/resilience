5ead4f610867a826cd8f0de05b6bdd2532dfe729
arthulian <xb8cbkp779@privaterelay.appleid.com>
2026-01-28 19:54:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ function CollapsibleCard({ card, isOpen, onToggle }: { card: CardData; isOpen: b
         </div>
       </button>
       
-                  {/* Collapsible Content */}
+      {/* Collapsible Content */}
       <div 
         className="collapsible-content"
         data-state={isOpen ? 'open' : 'closed'}
@@ -160,6 +160,9 @@ function CollapsibleCard({ card, isOpen, onToggle }: { card: CardData; isOpen: b
           </div>
         </div>
       </div>
+    </div>
+  )
+}
 
 function App() {
   const [openCards, setOpenCards] = useState<Set<string>>(new Set())
